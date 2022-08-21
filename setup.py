a18dbb7434f2d0b2a101c1fe44fc4dee4920ff2d
MatanShvide <92575645+MatanShvide@users.noreply.github.com>
2022-08-21 17:40:30 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-
-from setuptools import setup, Extension
-
-setup(name='mykmeanssp',
-      version='1.0',
-      description='mykmeanssp setup.py',
-      ext_modules=[Extension('mykmeanssp', sources=['spkmeans.c', 'spkmeansmodule.c'])])
-
-# the extension name is like the name in the signaure: PyInit_XXX(void)   (a function in spkmeansmoudle.c)
